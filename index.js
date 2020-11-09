ba1acaa7214022765ed2b0b986adc63c8b3602bd
Charlie Gador <charlie.gador@skola.botkyrka.se>
2020-11-09 15:57:23 +0100

    1-3

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
 const MAX_NUMBER = 100
 const MIN_NUMBER = 0
 
+
 let guess
 
 document.getElementById('too-high-btn').addEventListener('click', function () {
     // Vad ska hända när man tryckt too high?
+    guess--
+    setMessage(`Is it ${guess}?`)
 
+})
+
+document.getElementById('is-correct-btn').addEventListener('click', function () {
+    setMessage("Woho!")
 })
 
 document.getElementById('too-low-btn').addEventListener('click', function () {
     // Vad ska hända när man tryckt too low?
-    
+    guess++
+    setMessage(`Is it ${guess}?`)
+
 })
 
 
 function setMessage(msg) {
+    console.log("Nu är vi i setMessage")
     document.getElementById('message').innerText = msg
 }
 
 function start() {
     document.getElementById('before-start').style.display = 'none'
     document.querySelector('main').style.display = 'block'
+    
+    guess = Math.floor(Math.random() * 101)
 
-    guess = 0
-
-    // setMessage(`Is it ${guess}?`)
+    setMessage(`Is it ${guess}?`)
 }
+globalThis.setMessage = setMessage;
 
 document
     .getElementById('start-btn')
